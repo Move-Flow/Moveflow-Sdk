dacf4062f99151a644b7e4367a5d3cb9dc2b3fdd
Terry <bengbengle@gmail.com>
2023-05-19 12:02:00 +0800

    add demo

---
+++ README.md
@@ -0,0 +1,33 @@
+## Getting Started
+
+Create a project using this example:
+
+```bash
+yarn add @moveflow/sdk.js
+```
+
+You can start editing the page by modifying `src/App.tsx`. The page auto-updates as you edit the file.
+
+### Deploy to IPFS
+
+Deploy a copy of your application to IPFS using the following command:
+
+```bash
+yarn deploy
+```
+
+## Learn More
+
+To learn more about thirdweb, React and CRA, take a look at the following resources:
+
+- [thirdweb React Documentation](https://docs.thirdweb.com/react) - learn about our React SDK.
+- [thirdweb TypeScript Documentation](https://docs.thirdweb.com/react) - learn about our JavaScript/TypeScript SDK.
+- [thirdweb Portal](https://docs.thirdweb.com/react) - check our guides and development resources.
+- [Create React App Documentation](https://facebook.github.io/create-react-app/docs/getting-started) - learn about CRA features.
+- [React documentation](https://reactjs.org/) - learn React.
+
+You can check out [the thirdweb GitHub organization](https://github.com/thirdweb-dev) - your feedback and contributions are welcome!
+
+## Join our Discord!
+
+For any questions, suggestions, join our discord at [https://discord.gg/thirdweb](https://discord.gg/thirdweb).
---
+++ examples/demo/src/App.js
@@ -0,0 +1,149 @@
+import './App.css'
+import SDK, { NetworkType } from '@moveflow/sdk.js'
+
+import styled from 'styled-components/macro'
+const AptosCoin = '0x1::aptos_coin::AptosCoin'
+
+const Wrapper = styled.div`
+  position: relative;
+  padding: 8px;
+`
+
+const Button = styled.div`
+  position: relative;
+  padding: 8px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: nowrap;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  z-index: 1;
+`
+
+const rpc = 'https://testnet.aptoslabs.com'
+
+const sdk = new SDK(rpc, NetworkType.Testnet)
+
+
+const SignAndSubmitTransaction = async (transaction) => {
+
+  const payload = Object.assign({}, transaction)
+
+  await window.aptos.connect()
+  console.log('Petra tx', payload)
+  const pendingTransaction = await window.aptos.signAndSubmitTransaction(payload)
+  console.log(pendingTransaction)
+  return pendingTransaction.hash
+}
+
+
+function App() {
+
+  const create = async () => {
+    const start_time = Math.floor(Date.now() / 1000).toString();
+    const stop_time = Math.floor(Date.now() / 1000 + 60 * 60 * 24).toString();
+
+    const payload = sdk.stream.create({
+
+      recipientAddr: '0x20f0cbe21cb340fe56500e0889cad03f8a9e54a33e3c4acfc24ce2bdfabc4180',
+      depositAmount: 1,
+      startTime: start_time,
+      stopTime: stop_time,
+      // coinType: AptosCoin,
+      interval: 1000,
+      // name: '1',
+      // remark: '1',
+      // canPause: true,
+      // closeable: true,
+      // recipientModifiable: true
+
+    })
+
+    const txid = await SignAndSubmitTransaction(payload)
+
+    console.log('txid', txid)
+  }
+
+  const pause = async () => {
+
+    const payload = sdk.stream.pause({
+      id: 29,
+      coinType: AptosCoin,
+    })
+
+    const txid = await SignAndSubmitTransaction(payload)
+
+    console.log('txid', txid)
+  }
+
+  const resume = async () => {
+
+    const payload = sdk.stream.resume({
+      id: 29,
+      coinType: AptosCoin,
+    })
+
+    const txid = await SignAndSubmitTransaction(payload)
+
+    console.log('txid', txid)
+  }
+
+  const close = async () => {
+
+    const payload = sdk.stream.close({ id: 29 })
+
+    const txid = await SignAndSubmitTransaction(payload)
+
+    console.log('txid', txid)
+  }
+
+  const extend = async () => {
+
+    const payload = sdk.stream.extend({
+      id: 30,
+      extraAmount: 300,
+      stopTime: '1635724800',
+      ratePerInterval: '100',
+      interval: '1000',
+      coinType: AptosCoin,
+    })
+
+    const txid = await SignAndSubmitTransaction(payload)
+
+    console.log('txid', txid)
+  }
+
+  const incoming = async () => {
+
+    const res = await sdk.stream.getIncomingStreams('0x20f0cbe21cb340fe56500e0889cad03f8a9e54a33e3c4acfc24ce2bdfabc4180')
+
+    console.log('res: ', res)
+  }
+
+  const outgoing = async () => {
+
+    const res = await sdk.stream.getOutgoingStreams('0x20f0cbe21cb340fe56500e0889cad03f8a9e54a33e3c4acfc24ce2bdfabc4180')
+
+    console.log('res: ', res)
+
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Wrapper id="swap-page">
+          <Button onClick={create}>Create</Button>
+          <Button onClick={pause}>Pause</Button>
+          <Button onClick={resume}>Resume</Button>
+          <Button onClick={close}>Close</Button>
+          <Button onClick={extend}>Extend</Button>
+          <Button onClick={incoming}>Incoming</Button>
+          <Button onClick={outgoing}>Outgoing</Button>
+        </Wrapper>
+      </header>
+    </div>
+  );
+}
+
+export default App;
